f3289d3e088f2517d226f89a80bd6010125605ed
Ema <ema62x@gmail.com>
2024-09-20 12:35:52 -0300

    se cambiaron los nombres a los imports

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../components/header'
+import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 import Nav from '../components/Nav'
 
 function MainLayout() {
